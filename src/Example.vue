6916283a8e3810e6217f3e2990acbbe2e58d40fd
FilipeDoria <filipe0doria@gmail.com>
2021-09-05 21:25:12 +0100

    add pwa files

---
+++ src/Example.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <p>This button has {{ likes }} likes!</p>
+
+    <button @click="likeAndSubscribe()">Like & Subscribe</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+    };
+  },
+
+  methods: {
+    likeAndSubscribe() {
+      this.likes++;
+      alert("Thanks! Have some cake");
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+//import App from "./App.vue";
+import Example from "./Example.vue";
 import "./registerServiceWorker";
 
-createApp(App).mount("#app");
+createApp(Example).mount("#app");
