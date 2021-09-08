bc6d918e2a97dbf089207967a8d2c3f891677a3a
FilipeDoria <filipe0doria@gmail.com>
2021-09-08 11:11:06 +0100

    deploy

---
+++ precache-manifest.66bf2ca08396e34cb21c003ae038e4b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8494b6bd4ab7dfd9673",
+    "url": "/pwa-report/css/app.c5d99a75.css"
+  },
+  {
+    "revision": "c195ef7c656fa9bc595f444bd6f50498",
+    "url": "/pwa-report/index.html"
+  },
+  {
+    "revision": "d8494b6bd4ab7dfd9673",
+    "url": "/pwa-report/js/app.3673b30d.js"
+  },
+  {
+    "revision": "2cbf5660a07f93b9f01b",
+    "url": "/pwa-report/js/chunk-vendors.6ca80654.js"
+  },
+  {
+    "revision": "31ab0aa96bc02f3abb654b91b25835d3",
+    "url": "/pwa-report/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/pwa-report/robots.txt"
+  }
+]);
